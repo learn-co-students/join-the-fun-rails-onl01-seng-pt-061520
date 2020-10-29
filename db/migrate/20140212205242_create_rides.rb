b0bc9d09d81e32034e86642137566a1337978db4
sarisgar28 <sarisgar28@gmail.com>
2020-10-29 16:48:15 -0400

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,8 +1,6 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-      t.string :taxi
-      t.string :passenger
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20201029204602) do
 
   create_table "passengers", force: :cascade do |t|
-    t.integer  "taxi_id"
-    t.integer  "ride_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "rides", force: :cascade do |t|
-    t.string   "taxi"
-    t.string   "passenger"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
-    t.integer  "passenger_id"
-    t.string   "ride_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
